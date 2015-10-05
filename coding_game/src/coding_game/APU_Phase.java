f2fda09ddf28752e67a2ad14b544cf80f70b1bbe
Good_Listener <zigzaggg07@gmail.com>
2015-10-05 20:08:33 +0900

    10/5

--- coding_game/src/coding_game/APU_Phase.java
+++ coding_game/src/coding_game/APU_Phase.java
@@ -3,35 +3,48 @@ package coding_game;
 import java.util.Scanner;
 
 public class APU_Phase {
-  
+
   public static void main(String args[]) {
     Scanner in = new Scanner(System.in);
-    int width = in.nextInt(); // the number of cells on the X axis
-    in.nextLine();
-    int height = in.nextInt(); // the number of cells on the Y axis
-    in.nextLine();
+    int width = 4;
+    int height = 4;
 
-    char[][] phase = new char[width][height];
+    char[][] phase = new char[height][width];
     String[] answer = new String[height];
-    
     for (int i = 0; i < height; i++) {
-      String line = in.nextLine(); // width characters, each either 0 or .
-      
-      // 2차원 배열
-      // width 가로
-      // height 세로
-      // 00
-      // 0.
-      for(int j = 0; j < width; j++) {
-        phase[i] = line.toCharArray();
-        if(phase[i][j] == '0'){
-          answer[i] = (i + " " + j);
-        }
-          
+      if (i==0) {
+        String line = "00.0";
+        \n000."; 
       }
-      
-      System.out.print(answer[i]);
-
+      else if (i==1) {
+        String line = "0.00";.0.0
+      }
+      else if (i==2) {
+        String line = ".0.0";
+      }
+      phase[i] = line.toCharArray();
     }
+    System.out.println(phase[0]);
+    
   }
 }
+
+  /*
+   *  for(int j = 0; j < width; j++) {
+        if (phase[i][j] == '0') {
+          answer[i] = (answer[i] + i + " " + j);
+          check++;
+        }//하나값 입력되면 check 1증가
+        else if(phase[i][j] == '.' && j <= width && check < 3) {
+
+        }
+
+        else if (phase[i][j]== '.' && j <= width && chech < 3) {
+          answer[i] = answer[i] +"-1 -1";
+        }
+
+      }
+
+      System.out.print(answer[i]);
+
+   */
