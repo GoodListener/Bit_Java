eb67e0e278bbdcb31a9e3737829e7f95ec1a18be
Good_Listener <zigzaggg07@gmail.com>
2015-12-10 23:44:18 +0900

    ,

---
+++ gift/src/main/webapp/js/script.js
@@ -0,0 +1,112 @@
+(function($) {
+
+  $.fn.menumaker = function(options) {
+      
+      var cssmenu2 = $(this), settings = $.extend({
+        title: "Menu",
+        format: "dropdown",
+        breakpoint: 768,
+        sticky: false
+      }, options);
+
+      return this.each(function() {
+        cssmenu2.find('li ul').parent().addClass('has-sub');
+        if (settings.format != 'select') {
+          cssmenu2.prepend('<div id="menu-button">' + settings.title + '</div>');
+          $(this).find("#menu-button").on('click', function(){
+            $(this).toggleClass('menu-opened');
+            var mainmenu = $(this).next('ul');
+            if (mainmenu.hasClass('open')) { 
+              mainmenu.hide().removeClass('open');
+            }
+            else {
+              mainmenu.show().addClass('open');
+              if (settings.format === "dropdown") {
+                mainmenu.find('ul').show();
+              }
+            }
+          });
+
+          multiTg = function() {
+            cssmenu2.find(".has-sub").prepend('<span class="submenu-button"></span>');
+            cssmenu2.find('.submenu-button').on('click', function() {
+              $(this).toggleClass('submenu-opened');
+              if ($(this).siblings('ul').hasClass('open')) {
+                $(this).siblings('ul').removeClass('open').hide();
+              }
+              else {
+                $(this).siblings('ul').addClass('open').show();
+              }
+            });
+          };
+
+          if (settings.format === 'multitoggle') multiTg();
+          else cssmenu2.addClass('dropdown');
+        }
+
+        else if (settings.format === 'select')
+        {
+          cssmenu2.append('<select style="width: 100%"/>').addClass('select-list');
+          var selectList = cssmenu2.find('select');
+          selectList.append('<option>' + settings.title + '</option>', {
+                                                         "selected": "selected",
+                                                         "value": ""});
+          cssmenu2.find('a').each(function() {
+            var element = $(this), indentation = "";
+            for (i = 1; i < element.parents('ul').length; i++)
+            {
+              indentation += '-';
+            }
+            selectList.append('<option value="' + $(this).attr('href') + '">' + indentation + element.text() + '</option');
+          });
+          selectList.on('change', function() {
+            window.location = $(this).find("option:selected").val();
+          });
+        }
+
+        if (settings.sticky === true) cssmenu2.css('position', 'fixed');
+
+        resizeFix = function() {
+          if ($(window).width() > settings.breakpoint) {
+            cssmenu2.find('ul').show();
+            cssmenu2.removeClass('small-screen');
+            if (settings.format === 'select') {
+              cssmenu2.find('select').hide();
+            }
+            else {
+              cssmenu2.find("#menu-button").removeClass("menu-opened");
+            }
+          }
+
+          if ($(window).width() <= settings.breakpoint && !cssmenu2.hasClass("small-screen")) {
+            cssmenu2.find('ul').hide().removeClass('open');
+            cssmenu2.addClass('small-screen');
+            if (settings.format === 'select') {
+              cssmenu2.find('select').show();
+            }
+          }
+        };
+        resizeFix();
+        return $(window).on('resize', resizeFix);
+
+      });
+  };
+})(jQuery);
+
+(function($){
+$(document).ready(function(){
+
+$(document).ready(function() {
+  $("#cssmenu2").menumaker({
+    title: "Menu",
+    format: "dropdown"
+  });
+
+  $("#cssmenu2 a").each(function() {
+  	var linkTitle = $(this).text();
+  	$(this).attr('data-title', linkTitle);
+  });
+});
+
+});
+})(jQuery);
---
+++ gift/src/main/webapp/startPage.jsp
@@ -0,0 +1,33 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>    
+<!doctype html>
+<html>
+<head>
+   <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="./css/styles.css">
+   <link rel="stylesheet" href="./css/styles2.css">
+   <link rel="stylesheet" href="./css/common.css">
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="./js/script.js"></script>
+   <title>CSS MenuMaker</title>
+</head>
+<body>
+<jsp:include page="Header.html"></jsp:include>
+<jsp:include page="menu.html"></jsp:include>
+
+
+<div style="position: absolute; top:100px; left:220px;"> <!-- 여기 CSS 처리하기 -->
+		<h1>여기부터 테이블 시작</h1>
+		<ul>
+			
+		</ul>
+
+	</div>
+
+
+
+</body>
+</html>
